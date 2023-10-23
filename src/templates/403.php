70c698aeab191c4e7d4210efd5565c9b1ee9743f
mchauveni <matheorigami@gmail.com>
2023-10-23 19:22:16 +0200

    Added 403 template

---
+++ src/templates/403.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Page Introuvable</title>
+    <!-- Meta Tags -->
+    <meta name="robots" content="noindex, nofollow">
+    <!-- Favicon -->
+    <link rel="icon" href="./favicon.ico">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="./assets/css/login.css">
+</head>
+
+<body>
+    <h1>404 Forbidden !</h1>
+</body>
+
+</html>
+<?php
