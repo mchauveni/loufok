d18310ddfeb99342b83bef3aac1b864db23308dc
mchauveni <matheorigami@gmail.com>
2023-10-25 23:45:54 +0200

    Linking bdd

--- src/config/config.prod.php
+++ src/config/config.prod.php
@@ -3,16 +3,16 @@
 /* Fichier : Config/config.prod.php */
 
 // le nom de la base
-define('APP_DB_NAME', '/////');
+define('APP_DB_NAME', 'mchauveni_loufok');
 
 // le nom de la machine qui héberge le serveur MYSQL
-define('APP_DB_HOST', '/////');
+define('APP_DB_HOST', 'mysql-mchauveni.alwaysdata.net');
 
 // le nom de l'utilisateur MYSQL
-define('APP_DB_USER', '/////');
+define('APP_DB_USER', 'mchauveni');
 
 // le mot de passe de l'utilisateur MYSQL
-define('APP_DB_PASSWORD', '/////');
+define('APP_DB_PASSWORD', 'alwaysdataMotDePasseSecure!!!');
 
 // le préfixe de la table
 define('APP_TABLE_PREFIX', '');
