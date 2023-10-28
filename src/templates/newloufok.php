34350c3bb041d74261f29f19e760ab0208abf147
mchauveni <matheorigami@gmail.com>
2023-10-28 02:01:54 +0200

    new loufok works (still 1 bug)

--- src/templates/newloufok.php
+++ src/templates/newloufok.php
@@ -1,34 +1,60 @@
 <?php
 
-// Récupère la loufokerie et formate les dates
-$loufokerie = Loufokerie::getInstance()->getCurrent();
+$errors = null;
+if (HTTP::is_method_post()) {
 
-if ($loufokerie) {
-    // Les dates de la loufokerie en cours
-    $date_debut = date_format(date_create($loufokerie['date_debut_loufokerie']), 'd M Y');
-    $date_fin = date_format(date_create($loufokerie['date_fin_loufokerie']), 'd M Y');
-
-    // Récupère toutes les contributions de la loufokerie actuelle
-    $contributions = Contribution::getInstance()->findBy(['id_loufokerie' => $loufokerie['id_loufokerie']]);
-
-    // Récupère la contribution aléatoire du joueur
-    $random_contribution = RandomContribution::getInstance()->findBy(['id_joueur' => $_COOKIE["id"], 'id_loufokerie' => $loufokerie["id_loufokerie"]]);
-    $random_contribution = ($random_contribution != null) ? Contribution::getInstance()->findBy(["id_contribution" => $random_contribution[0]['id_contribution']])[0] : $random_contribution;
-
-    // Récupère la contribution du joueur, si elle existe
-    $user_contribution = Contribution::getInstance()->findBy(["id_loufokerie" => $loufokerie["id_loufokerie"], "id_joueur" => $_COOKIE["id"]]);
-    $user_contribution = (count($user_contribution) > 0) ? $user_contribution[0] : null;
-}
-
-function txtContribSingularPlural($nb) {
-    if ($nb <= 0) {
-        $txt = null;
-    } else if ($nb == 1) {
-        $txt = "<p class='hidden_contribution'><span class='hidden_contribution__text'>$nb contribution masquée</span></p>";
+    // Si tous les champs ne sont pas remplis, on ignore les autres tests
+    if (!isset($_POST['titre_loufokerie']) || !isset($_POST['date_debut_loufokerie']) || !isset($_POST['heure_debut_loufokerie']) || !isset($_POST['date_fin_loufokerie']) || !isset($_POST['heure_fin_loufokerie']) || !isset($_POST['texte_contribution'])) {
+        $errors = "Tous les champs ne sont pas remplis";
     } else {
-        $txt = "<p class='hidden_contribution'><span class='hidden_contribution__text'>$nb contributions masquées</span></p>";
+        $datetime_debut = new DateTime($_POST['date_debut_loufokerie'] . " " . $_POST['heure_debut_loufokerie']);
+        $datetime_debut = $datetime_debut->format('Y-m-d H:i:s');
+
+        $datetime_fin = new DateTime($_POST['date_fin_loufokerie'] . " " . $_POST['heure_fin_loufokerie']);
+        $datetime_fin = $datetime_fin->format('Y-m-d H:i:s');
+
+        $now = new DateTime('now');
+        $now = $now->format('Y-m-d H:i:s');
+
+        // L'utilisateur ne peux pas créer de Loufokerie en dehors de MAINTENANT (pas prévoir)
+        if ($datetime_debut > $now) {
+            $errors = "Date de début ultérieure à maintenant";
+        }
+
+        if ($datetime_fin < $now) {
+            $errors = "Date de début antérieure à maintenant";
+        }
+
+        var_dump($_POST['texte_contribution']);
+        if (strlen($_POST['texte_contribution']) < 50) {
+            $errors = "Texte trop court";
+        }
+        if (strlen($_POST['texte_contribution']) > 280) {
+            $errors = "Texte trop long";
+        }
     }
-    return $txt;
+
+
+    if (!$errors) {
+        $id = Loufokerie::getInstance()->create([
+            "id_administrateur" => $_COOKIE['id'],
+            "titre_loufokerie" => $_POST["titre_loufokerie"],
+            "date_debut_loufokerie" => $_POST["date_debut_loufokerie"],
+            "date_fin_loufokerie" => $_POST["date_fin_loufokerie"],
+            "nb_contributions" => 0,
+            "nb_jaime" => 0,
+        ]);
+
+        Contribution::getInstance()->createSubmission([
+            "id_loufokerie" => $id,
+            "id_administrateur" => $_COOKIE['id'],
+            "texte_contribution" => $_POST['texte_contribution'],
+            "date_soumission" => $now,
+            "ordre_soumission" => 1,
+        ]);
+
+        HTTP::redirect('/admin');
+    }
 }
 ?>
 
@@ -50,7 +76,7 @@ function txtContribSingularPlural($nb) {
     <link rel="stylesheet" href="./assets/css/header.css">
     <link rel="stylesheet" href="./assets/css/newloufok.css">
     <!-- JS -->
-    <script src="/assets/scripts/contribution_textarea_handler.js"></script>
+    <script src="./assets/scripts/contribution_textarea_handler.js"></script>
 </head>
 
 <body>
@@ -64,23 +90,32 @@ function txtContribSingularPlural($nb) {
         </hr>
     </header>
 
-    <form class="form" action="">
+    <form class="form" method="POST">
         <div class="form__div">
             <label class="form__label" for="titre_loufokerie">Titre</label>
-            <input class="form__input" type="text" name="titre_loufokerie" id="">
+            <input class="form__input" type="text" name="titre_loufokerie" required>
         </div>
         <div class="form__div">
             <label class="form__label" for="date_debut_loufokerie">Date de début</label>
             <div class="form__inputwrapper">
-                <input class="form__input" type="date" name="date_debut_loufokerie" id="">
-                <input class="form__input" type="time" name="" id="">
+                <input class="form__input" type="date" name="date_debut_loufokerie" required>
+                <input class="form__input" type="time" name="heure_debut_loufokerie" aria-label="heure de début" required>
             </div>
             <label class="form__label" for="date_fin_loufokerie">Date de fin</label>
             <div class="form__inputwrapper">
-                <input class="form__input" type="date" name="date_fin_loufokerie" id="">
-                <input class="form__input" type="time" name="" id="">
+                <input class="form__input" type="date" name="date_fin_loufokerie" required>
+                <input class="form__input" type="time" name="heure_fin_loufokerie" aria-label="heure de fin" required>
             </div>
         </div>
+        <div class="form__div">
+            <label class="form__label" for="texte_contribution">Première contribution</label>
+            <textarea class="form__input contribute__input" name="texte_contribution" required></textarea>
+        </div>
+        <?php
+        if ($errors) {
+            echo "<p class='errors'>$errors</p>";
+        }
+        ?>
         <div class="form__submit">
             <button class="form__button">Valider</button>
         </div>
