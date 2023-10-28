98063c637058294ee5dc4cce90cb33be612fc40a
mchauveni <matheorigami@gmail.com>
2023-10-28 02:03:11 +0200

    need to fix this, it's broken

--- src/router.php
+++ src/router.php
@@ -11,20 +11,22 @@ $route = $route === '' ? '/' : $route;
 foreach ($routes as $r) {
     if (in_array($route, $r['route']) && in_array($_SERVER['REQUEST_METHOD'], $r['method'])) {
         if (!isset($_COOKIE['is_logged_in'])) {
-            setcookie("is_logged_in", '0');
+            setcookie("is_logged_in", 0);
+            HTTP::redirect('/login');
         }
-
 
         if (!$_COOKIE['is_logged_in'] && $route != "/login") {
             HTTP::redirect('/login');
         }
 
-        if (isset($r['auth']) && isset($_COOKIE['account_type'])) {
+        /*         if (isset($r['auth']) && $_COOKIE['account_type'] && $_COOKIE['is_logged_in']) {
             if ($r['auth'] != $_COOKIE['account_type']) {
                 require 'templates/403.php';
                 exit;
+            } else if ($r['auth'] == $_COOKIE['account_type']) {
+                exit;
             }
-        }
+        } */
 
         require 'templates/' . $r['script'];
         exit;
